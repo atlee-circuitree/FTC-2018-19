00c949624a73356d9469f9debc55f159420db4ae
Chris Berg <cubeberg@gmail.com>
2018-10-30 14:12:00 -0400

    Adding code comments - putting in delay for combine servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2018Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2018Drive.java
@@ -41,14 +41,15 @@ import com.qualcomm.robotcore.util.Range;
  * Drives arm motors with a controller for testing and positioning the robot for measuring
  */
 
-@TeleOp(name = "Arm Motor Drive", group = "Linear Opmode")
+@TeleOp(name = "TeleOp2018Manual", group = "Linear Opmode")
 
-public class ArmMotorDrive extends LinearOpMode {
+public class TeleOp2018Drive extends LinearOpMode {
 
 
     // Declare OpMode members.
     hardware2018 robot = new hardware2018();
     private ElapsedTime runtime = new ElapsedTime();
+
 
     @Override
     public void runOpMode() {
@@ -64,16 +65,17 @@ public class ArmMotorDrive extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
+        // Setup a variable for each drive wheel to save power level for telemetry
+        double leftPower;
+        double rightPower;
+        double combineSpeed = 0;
+        double armCombineOpenEndTime = 0;
+
+
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            // Setup a variable for each drive wheel to save power level for telemetry
-            double leftPowerF;
-            double leftPowerR;
-            double rightPowerF;
-            double rightPowerR;
 
-            double combineSpeed = 0;
             // Choose to drive using either Tank Mode, or POV Mode
             // Comment out the method that's not used.  The default below is POV.
 
@@ -81,57 +83,8 @@ public class ArmMotorDrive extends LinearOpMode {
             // - This uses basic math to combine motions and is easier to drive straight.
             double drive = -gamepad1.left_stick_y;
             double turn = gamepad1.right_stick_x;
-            leftPowerF = Range.clip(drive + turn, -0.7, 0.7);
-            leftPowerR = Range.clip(drive + turn, -0.7, 0.7);
-            rightPowerF = Range.clip(drive - turn, -0.7, 0.7);
-            rightPowerR = Range.clip(drive - turn, -0.7, 0.7);
-
-
-            //get data for combine speed
-            if (!gamepad1.left_bumper && !gamepad1.right_bumper) {
-                combineSpeed = 0;
-            } else if (gamepad1.left_bumper) {
-                combineSpeed = 1;
-            } else if (gamepad1.right_bumper) {
-                combineSpeed = -1;
-            }
-
-
-            //move arm in and out
-            if (gamepad2.left_stick_y > 0.5)
-                robot.armExtendMotor.setPower(-1);
-            else if (gamepad2.left_stick_y < -0.5)
-                robot.armExtendMotor.setPower(1);
-            else
-                robot.armExtendMotor.setPower(0);
-
-            //move arm up and down
-            if (gamepad2.right_stick_y > 0.5)
-                robot.armJointMotor.setPower(1);
-            else if (gamepad2.right_stick_y < -0.5)
-                robot.armJointMotor.setPower(-1);
-            else
-                robot.armJointMotor.setPower(0);
-
-            if (gamepad2.left_trigger > 0)
-                robot.armCombineServo.setPosition(0);
-            else if (gamepad2.right_trigger > 0)
-                robot.armCombineServo.setPosition(0.5);
-
-            if (gamepad1.y)
-                robot.climbMotor.setPower(1);
-            else if (gamepad1.a)
-                robot.climbMotor.setPower(-1);
-            else
-                robot.climbMotor.setPower(0);
-
-            if (gamepad1.x) {
-                robot.armReleaseServo.setPwmEnable();
-                robot.armReleaseServo.setPosition(0);
-            } else {
-                robot.armReleaseServo.setPwmDisable();
-                //robot.armReleaseServo.setPosition(1);
-            }
+            leftPower = Range.clip(drive + turn, -1, 1);
+            rightPower = Range.clip(drive - turn, -1, 1);
 
             // Tank Mode uses one stick to control each wheel.
             // - This requires no math, but it is hard to drive forward slowly and keep straight.
@@ -139,12 +92,73 @@ public class ArmMotorDrive extends LinearOpMode {
             // rightPower = -gamepad1.right_stick_y ;
 
             // Send calculated power to wheels
-            robot.leftDriveFront.setPower(leftPowerF);
-            robot.leftDriveRear.setPower(leftPowerR);
-            robot.rightDriveFront.setPower(rightPowerF);
-            robot.rightDriveRear.setPower(rightPowerR);
+            robot.leftDriveFront.setPower(leftPower);
+            robot.leftDriveRear.setPower(leftPower);
+            robot.rightDriveFront.setPower(rightPower);
+            robot.rightDriveRear.setPower(rightPower);
 
             robot.armCombineMotor.setPower(combineSpeed);
+
+            //get data for combine speed
+            //starts off, stays on in whatever the last direction was
+            if (gamepad1.left_bumper) {
+                combineSpeed = 1;
+            } else if (gamepad1.right_bumper) {
+                combineSpeed = -1;
+            }
+
+
+            //move arm in and out
+            if (gamepad2.left_stick_y > 0.5)  //stick values are -1 to 1 - so we have to set a "threshold" that triggers movement
+                robot.armExtendMotor.setPower(-1);
+            else if (gamepad2.left_stick_y < -0.5)
+                robot.armExtendMotor.setPower(1);
+            else
+                robot.armExtendMotor.setPower(0);
+
+            //move arm up and down
+            if (gamepad2.right_stick_y > 0.5)  //stick values are -1 to 1 - so we have to set a "threshold" that triggers movement
+                robot.armJointMotor.setPower(1);
+            else if (gamepad2.right_stick_y < -0.5)
+                robot.armJointMotor.setPower(-1);
+            else
+                robot.armJointMotor.setPower(0);
+
+            //open and close the combine's servo to hold/release game pieces - option 1 - open and close triggered by buttons
+            //if (gamepad2.left_trigger > 0)
+            //    robot.armCombineServo.setPosition(0);
+            //else if (gamepad2.right_trigger > 0)
+            //    robot.armCombineServo.setPosition(0.5);
+
+            //open and close the combine's servo to hold/release game pieces - option 2 - open triggered by a button, close after a specific timeframe
+            //first time the button is pushed - set a timestamp for when the servo should go back into position
+            if (gamepad1.a && armCombineOpenEndTime == 0) {
+                armCombineOpenEndTime = runtime.milliseconds() + 2000; //right now + 2 seconds.  We use milliseconds instead of seconds to avoid rounding problems
+                robot.armCombineServo.setPosition(0);
+            }
+            if (armCombineOpenEndTime < runtime.milliseconds()) //have our 2 seconds passed?
+            {
+                armCombineOpenEndTime = 0;
+                robot.armCombineServo.setPosition(0.5);
+            }
+
+            //drive the climbing/drop motor
+            if (gamepad1.y)
+                robot.climbMotor.setPower(1);
+            else if (gamepad1.a)
+                robot.climbMotor.setPower(-1);
+            else
+                robot.climbMotor.setPower(0);
+
+            //trigger the arm release servo - can be used in-game in case of emergency,
+            //or to manually go through the motions of start-up without autonomous code
+            if (gamepad1.x) {
+                robot.armReleaseServo.setPwmEnable();
+                robot.armReleaseServo.setPosition(0);
+            } else {
+                robot.armReleaseServo.setPwmDisable();
+                //robot.armReleaseServo.setPosition(1);
+            }
 
 
             // Show the elapsed game time and wheel power.
