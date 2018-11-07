e1664b7e4e35e26428fbcb9ac6fbbd7afc1430cf
retiform <colerogers@protonmail.com>
2018-11-07 16:45:46 -0500

    Change basic auto name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basicAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/basicAuto.java
@@ -51,7 +51,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name = "basic - AutoStart", group = "Linear Opmode")
+@Autonomous(name = "basicAuto", group = "Linear Opmode")
 
 public class basicAuto extends LinearOpMode {
 
