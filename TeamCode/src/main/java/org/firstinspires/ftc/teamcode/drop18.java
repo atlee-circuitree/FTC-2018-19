016ac280e4d26f3a26d329fabfcf69db615f4c99
retiform <colerogers@protonmail.com>
2018-11-28 16:03:30 -0500

    Create dropcode which should be based on the stuff from the 2018 telemetry file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drop18.java
@@ -0,0 +1,115 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This is NOT an opmode.
+ * This class can be used to drop the 2018 bot
+ */
+
+
+//TODO: make thing work
+
+public class drop18 {
+
+    hardware2018 robot = new hardware2018();
+    MineralDetector mineralDetector = new MineralDetector();
+    private ElapsedTime runtime = new ElapsedTime();
+
+    /* Constructor */
+    public drop18() {
+    }
+
+    public void dropBot(){
+        robot.init(this, hardwareMap);
+        mineralDetector.init(hardwareMap, telemetry);
+	boolean dropStageCompleted = false;
+	int dropPosition = 21000;
+        int jointRaisePosition = 1800;  //1400 - better height for collecting
+        int extendOutPosition = 5000;
+	
+        //run climb motor until we've dropped
+while (robot.climbMotor.getCurrentPosition() < dropPosition) {
+            robot.climbMotor.setPower(1);
+        }
+        robot.StopAll();
+
+        //Timing based forward movement
+        runtime.reset();
+
+        robot.DriveTimed(DriveDirection.Forward, 200);
+
+        runtime.reset();
+
+        
+	//the dropping part
+        if (goldPosition == MineralDetector.MineralPosition.Center) { 
+            while (!dropStageCompleted) {
+                telemetry.addData("Status", "Run Time: " + runtime.toString());
+                telemetry.addData("Gold Position", goldPosition);
+
+                if (robot.armJointMotor.getCurrentPosition() >= jointRaisePosition
+                        && robot.armExtendMotor.getCurrentPosition() >= extendOutPosition
+                        && robot.climbMotor.getCurrentPosition() >= dropPosition) {
+                    dropStageCompleted = true;
+                }
+
+                if (runtime.milliseconds() < 1000)
+                    robot.armReleaseServo.setPosition(0);
+                else
+                    robot.armReleaseServo.setPwmDisable();
+
+                if (robot.armJointMotor.getCurrentPosition() < jointRaisePosition)
+                    robot.ArmJointRaise();
+                else
+                    robot.ArmJointStop();
+
+                if (robot.armExtendMotor.getCurrentPosition() < extendOutPosition)
+                    robot.ArmExtendOut();
+                else
+                    robot.ArmExtendStop();
+
+                // Show the elapsed game time and wheel power.
+                telemetry.update();
+            } 
+        } else {
+            while (opModeIsActive() && !dropStageCompleted) {
+                telemetry.addData("Status", "Run Time: " + runtime.toString());
+                telemetry.addData("Gold Position", goldPosition);
+
+                if (robot.armJointMotor.getCurrentPosition() >= jointRaisePosition
+                        && robot.armExtendMotor.getCurrentPosition() >= extendOutPosition
+                        && robot.climbMotor.getCurrentPosition() >= dropPosition) {
+                    dropStageCompleted = true;
+                }
+
+                if (runtime.milliseconds() < 1000)
+                    robot.armReleaseServo.setPosition(0);
+                else
+                    robot.armReleaseServo.setPwmDisable();
+
+                if (robot.armJointMotor.getCurrentPosition() < jointRaisePosition)
+                    robot.ArmJointRaise();
+                else
+                    robot.ArmJointStop();
+
+                if (runtime.milliseconds() > 2000 && robot.armExtendMotor.getCurrentPosition() < extendOutPosition)
+                    robot.ArmExtendOut();
+                else
+                    robot.ArmExtendStop();
+
+                // Show the elapsed game time and wheel power.
+                telemetry.update();
+            }
+        }
+
+        telemetry.addData("Gold Position", goldPosition);
+        telemetry.update();
+
+        robot.StopAll();
+	//end of dropping
+
+    }
+}
